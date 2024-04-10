fe9543c805c2affc179c68343c7d17bf656fbadd
pla54 <113071547+pla54@users.noreply.github.com>
2024-04-10 15:42:30 -0600

    auth

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,16 +2,21 @@ using INTEXteam5.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace INTEXteam5.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        // Modify the constructor to accept both ILogger<HomeController> and UserManager<IdentityUser>
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager; // Now userManager is defined as a parameter
         }
 
         public IActionResult Index()
@@ -28,6 +33,69 @@ namespace INTEXteam5.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminAction()
+        {
+            return View("Admin");
+        }
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult ViewUsers()
+        {
+            List<IdentityUser> users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+
+        //[HttpGet]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> EditRoles(string userId)
+        //{
+        //    var user = await _userManager.FindByIdAsync(userId);
+        //    var viewModel = new EditRoles
+        //    {
+        //        UserId = user.Id,
+        //        UserName = user.UserName,
+        //        Roles = _roleManager.Roles.Select(r => new UserRoleViewModel
+        //        {
+        //            RoleId = r.Id,
+        //            RoleName = r.Name,
+        //            IsSelected = _userManager.IsInRoleAsync(user, r.Name).Result
+        //        }).ToList()
+        //    };
+
+        //    return View(viewModel);
+        //}
+
+        //[HttpPost]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> EditRoles(EditRoles model)
+        //{
+        //    var user = await _userManager.FindByIdAsync(model.UserId);
+
+        //    foreach (var role in model.Roles)
+        //    {
+        //        if (role.IsSelected && !(await _userManager.IsInRoleAsync(user, role.RoleName)))
+        //        {
+        //            await _userManager.AddToRoleAsync(user, role.RoleName);
+        //        }
+        //        else if (!role.IsSelected && await _userManager.IsInRoleAsync(user, role.RoleName))
+        //        {
+        //            await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+        //        }
+        //    }
+
+        //    return RedirectToAction("ViewUsers"); // Or wherever you want to redirect
+        //}
+        ////public async Task<IActionResult> Delete(string id)
+        ////{
+        ////    IdentityUser user = await _userManager.FindByIdAsync(id);
+        ////    IdentityResult result = await _userManager.DeleteAsync(user);
+
+        ////    return RedirectToAction("ViewUsers");
+        ////}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,113 @@
+//using INTEXteam5.Data;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Sqlite;
+//using Microsoft.Extensions.Options;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.Configure<CookiePolicyOptions>(options =>
+//{
+//    // This lambda determines whether user consent for non-essential 
+//    // cookies is needed for a given request.
+//    options.CheckConsentNeeded = context => true;
+
+//    options.MinimumSameSitePolicy = SameSiteMode.None;
+//    options.ConsentCookieValue = "true";
+//});
+
+//// Add services to the container.
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    //options.UseSqlServer(connectionString));
+//    options.UseSqlite(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+//    .AddRoles<IdentityRole>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddControllersWithViews();
+
+//builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+//{
+//    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+//});
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseMigrationsEndPoint();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.Use(async (ctx, next) =>
+//{
+//    ctx.Response.Headers.Append("Content-Security-Policy", "default-src 'self' https://apis.google.com; script-src 'self' https://apis.google.com; style-src 'self' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com;");
+//    await next();
+//});
+
+
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseCookiePolicy();
+
+//app.UseRouting();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapRazorPages();
+
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//using(var scope = app.Services.CreateScope())
+//{
+//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//    var roles = new[] { "Admin", "Manager", "Member" };
+
+//    foreach(var role in roles)
+//    {
+//        if (!await roleManager.RoleExistsAsync(role))
+//            await roleManager.CreateAsync(new IdentityRole(role));
+//    }
+//}
+
+//var adminEmail = builder.Configuration["Admin:Email"];
+//var adminPassword = builder.Configuration["Admin:Password"];
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+//    string email = adminEmail;
+//    string password = adminPassword;
+
+//    if(await userManager.FindByEmailAsync(email) == null)
+//    {
+//        var user = new IdentityUser();
+//        user.UserName = email;
+//        user.Email = email;
+
+//        await userManager.CreateAsync(user, password);
+
+//        await userManager.AddToRoleAsync(user, "Admin");
+//    }
+//}
+
+//app.Run();
+
+
 using INTEXteam5.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -69,15 +179,12 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var roles = new[] { "Admin", "Manager", "Member" };
 
-    foreach(var role in roles)
+    foreach (var role in roles)
     {
         if (!await roleManager.RoleExistsAsync(role))
             await roleManager.CreateAsync(new IdentityRole(role));
@@ -93,7 +200,7 @@ using (var scope = app.Services.CreateScope())
     string email = adminEmail;
     string password = adminPassword;
 
-    if(await userManager.FindByEmailAsync(email) == null)
+    if (await userManager.FindByEmailAsync(email) == null)
     {
         var user = new IdentityUser();
         user.UserName = email;
@@ -105,4 +212,6 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
